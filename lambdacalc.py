1304231b510ecfde393a01d39fa365e82dffad14
harigovind <your_address_email@domain.com>
2020-12-02 23:18:21 +0530

    First commit

---
+++ lambdacalc.py
@@ -0,0 +1,52 @@
+a = True
+while a:
+
+
+    add = lambda a,b:a+b
+    sub = lambda a,b:a-b
+    mul = lambda a,b:a*b
+    div = lambda a,b:a/b
+
+
+    o1 = int(input("enter the first operand"))
+    o2 = int(input("enter the second operand"))
+
+    o = input("enter the operation")
+
+    if o == "add":
+
+         print(add(o1,o2))
+
+    if o =="sub":
+
+        print(sub(o1,o2))
+
+    if o =="mul":
+
+        print(mul(o1,o2))
+
+    if o =="div":
+
+        print(div(o1,o2))
+
+    if o == None:
+
+        print("enter a valid operation")
+
+a = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
